00021f135625f794cf899ae892d6f450a60403b1
abhimanyura <abhimanyu.ise@gmail.com>
2017-08-01 15:10:13 +0530

    master is branching to 2 user

--- Test_GIT/src/teest1.java
+++ Test_GIT/src/teest1.java
@@ -1,15 +1,5 @@
 
 public class teest1 {
 
-	
-	int i;
-	int j;
-	int k;
-	private void sysout() {
-		// TODO Auto-generated method stub
-System.out.println("1st checkin ");
-//aabhimanyu
-System.out.println("I am making a change from secod resource");
-System.out.println("Removing the abhimanyu id ");
-	}
+
 }
